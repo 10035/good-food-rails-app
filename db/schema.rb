ddf9323af482ac32201e76039c48a9dbbcb1ffe0
Luis R <looes1@gmail.com>
2021-09-13 02:01:34 +1000

    drop delete table model ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_155758) do
+ActiveRecord::Schema.define(version: 2021_09_12_160006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "ingredients", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "meals", force: :cascade do |t|
     t.string "name"
