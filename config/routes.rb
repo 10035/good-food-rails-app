c1ba84891dee46a15ee7470ae109b4a91e6fa3b1
Luis R <looes1@gmail.com>
2021-09-15 20:01:32 +1000

    add controller recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-root to: 'recommendations/new'
+  root to: 'recommendations#new'
 
-# /recommendations/new is the first page the user sees
-resources :recommendations, only [ :new ]
+  get 'recommendations/new'
+  # /recommendations/new is the first page the user sees
+  # get "/recommendations", to: "recommendations#new"
 
-# From there, user is able to generate a new set of meal recommendations 
+  # From there, user is able to generate a new set of meal recommendations 
 
 end
