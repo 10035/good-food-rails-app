14429f53ecd9e6a98407c3f8051e6da545e12c85
Luis R <looes1@gmail.com>
2021-09-16 08:42:02 +1000

    add routes to for show action on recomm controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'recommendations#new'
 
   get 'recommendations/new'
+  get 'recommendations/show'
   # /recommendations/new is the first page the user sees
   # get "/recommendations", to: "recommendations#new"
 
