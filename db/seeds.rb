2194b9c1194b6d402d4b19ed695bafa7d56d7214
Luis R <looes1@gmail.com>
2021-09-16 10:41:59 +1000

    add seed file generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "Creating meal recommendations..."
+
+# sushi = Meal.create(name: "Sushi")
+# pho = Meal.create(name: "Vietnamese Pho")
+# banh_mi = Meal.create(name: "Banh Mi")
+
+# puts "We created the following meals:" 
+# puts Meal.each do |meal| 
+#   Meal.all.name
+# end
+
+# puts "All done!"
+
+require 'faker'
+
+puts 'Creating 10 healthy meal options...'
+10.times do
+  meal = Meal.new(
+    name: Faker::Food.dish,
+  )
+  meal.save  
+end
+puts 'Created these healthy meals for you:'
+puts Meal.last(10) # TODO: find active record method query to retrieve name attribute from meal instance
+
+# puts Meal.find_each do |meal|
+#   Meal.name()
+# end
+
+
+
+puts 'Finished!'
